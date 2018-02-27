4e855ed1a3748970ea15a006ee0640d082d2a083
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-02-27 12:15:06 -0500

    Nearly done, waiting on sendgrid to respond to my email about an account issue I am having adding the sendgrid add-on to my account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,8 @@
-User.create!(name: "Test User", email: "test@test.com", password: "test12", password_confirmation: "test12", admin: true)
+User.create!(name: "Test User", email: "test@test.com", password: "test12", password_confirmation: "test12", admin: true, activated: true, activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+  User.create!(name:  name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
 end
