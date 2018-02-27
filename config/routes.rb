89a793145e170132e39cc70da5fc6bca0a45e152
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-02-27 16:14:19 -0500

    Finished reset password, was way easier due to everything else that was already done

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
 	resources :users
   resources :account_activations, only: [:edit]
-	
+	resources :password_resets, only: [:new, :create, :edit, :update]
+
 	get '/signup', to: 'users#new'
 	get '/contact', to:'static_pages#contact'
-  	get '/home', to: 'static_pages#home'
-  	get  '/help', to: 'static_pages#help'
-  	get  '/about', to: 'static_pages#about'
-  	get    '/login',   to: 'sessions#new'
+  get '/home', to: 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/about', to: 'static_pages#about'
+  get    '/login',   to: 'sessions#new'
   	
 	post '/signup',  to: 'users#create'
 	post   '/login',   to: 'sessions#create'
 	
-  	delete '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy'
 
-  	root 'static_pages#home'
+  root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
