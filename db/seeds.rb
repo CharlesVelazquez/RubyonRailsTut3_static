97eea0462254a06cdeb02d6495356434c24606fd
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-02-28 16:49:20 -0500

    Bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,15 @@ User.create!(name: "Test User", email: "test@test.com", password: "test12", pass
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
-  User.create!(name:  name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
-
 end
