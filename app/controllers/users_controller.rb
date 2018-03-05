13f592c103bcdcd45975e31e7829ee1375dfb78d
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-03-04 21:14:43 -0500

    If the code works with no bugs, I am now finished implementing the following and followers functionality and am now done with this site in terms of plans for up and running status, following test and fixes if any, this is complete.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user, only: :destroy#Wanted to keep attackers from deleting users from the cmd lines
 
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-    	flash[:success] = "Welcome to the Sample App!"
+    	flash[:success] = "Welcome to the VBlog!"
       redirect_to @user
     else
       render 'new'
@@ -46,6 +46,21 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+   def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
 private
   def user_params
