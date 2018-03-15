3933c835c88d2cfb5af7a0fa12074b23b43ad3bf
CharlesVelazquez <CharlesVelazquez15@gmail.com>
2018-03-15 12:42:19 -0400

    Forgot to change the code from the excercise question smh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy#Wanted to keep attackers from deleting users from the cmd lines
 
    def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
